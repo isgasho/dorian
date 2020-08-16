d8fd3b090feaecb3620260960e34bbf7b78aa8e4
Luiz de Milon <luiz.de.milon@gmail.com>
2020-08-16 11:34:38 -0400

    update (hopefully meaningful git commit messages will come soon)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
 use std::path::Path;
-use uuid::Uuid;
 
 extern crate serde;
 use serde::{Deserialize, Serialize};
@@ -50,15 +48,6 @@ struct User {
     name: String,
 }
 
-impl User {
-    fn new(n: String) -> User {
-        User {
-            name: n,
-            id: Uuid::new_v4().to_u128_le(),
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize, Identifiable, Queryable)]
 #[table_name = "entries"] // diesel thought this was "entrys", funny
 struct Entry {
@@ -67,45 +56,16 @@ struct Entry {
     uploader: String, // User.name
 }
 
-impl Entry {
-    fn new(u: User, ts: Vec<String>) -> Entry {
-        Entry {
-            id: Uuid::new_v4().to_u128_le(),
-            uploader: u.name,
-            tags: ts,
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize, Identifiable, Queryable)]
 struct Tag {
     id: u128,
     name: String,
-}
-
-impl Tag {
-    fn new(n: String) -> Tag {
-        Tag {
-            id: Uuid::new_v4().to_u128_le(),
-            name: n,
-        }
-    }
 }
 #[derive(Serialize, Deserialize, Identifiable, Queryable)]
 struct Taglink {
     id: u128,
     tag_id: u128,
     entry_id: u128,
-}
-
-impl Taglink {
-    fn new(tag: Tag, entry: Entry) -> Taglink {
-        Taglink {
-            id: Uuid::new_v4().to_u128_le(),
-            tag_id: tag.id,
-            entry_id: entry.id,
-        }
-    }
 }
 
 enum StorageError {
@@ -128,15 +88,14 @@ async fn main() {
     use schema::tags::dsl::*;
 
     let connection = SqliteConnection::establish("dorian.db").expect("error connecting to db");
-    let qr = tags
+
+    let ts = tags
         .select(name)
         .load::<String>(&connection)
-        .expect("Error loading tags");
+        .expect("Error loading tags")
+        .join(" ");
 
-    for tag in qr.iter() {
-        println!("{}", tag);
-    }
+    let t = warp::path!("tags").map(move || format!("{}", ts));
 
-    let health = warp::path!("health").map(|| "200 OK");
-    warp::serve(health).run(([0, 0, 0, 0], 3030)).await;
+    warp::serve(t).run(([0, 0, 0, 0], 3030)).await;
 }
